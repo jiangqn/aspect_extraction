a089bba84aaabf9173b16a4c7bab0e82a9b27124
1935448858@qq.com <1935448858@qq.com>
2019-01-30 13:28:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,39 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class TECNN(nn.Module):
 
-    def __init__(self):
-        pass
+    def __init__(self, embedding, dropout, layers):
+        super(TECNN, self).__init__()
+        self._embedding = embedding
+        self._dropout = nn.Dropout(dropout)
+        self._layers = nn.ModuleList()
+        input_size = embedding.embedding_dim
+        for layer_kernels in layers:
+            layer = nn.ModuleList()
+            output_size = 0
+            for kernel in layer_kernels:
+                conv = nn.Conv1d(
+                    in_channels=input_size,
+                    out_channels=kernel[0],
+                    kernel_size=kernel[1],
+                    padding=kernel[1] // 2
+                )
+                layer.append(conv)
+                output_size += kernel[0]
+            self._layers.append(layer)
+            input_size = output_size
 
-    def forward(self):
-        pass
+    def forward(self, src, src_lens):
+        src = self._embedding(src)
+        src = self._dropout(src).transpose(1, 2)
+        for layer in self._layers:
+            layer_outputs = []
+            for conv in layer:
+                output = F.relu(conv(src))
+                layer_outputs.append(output)
+            src = torch.cat(layer_outputs, dim=1)
+            src = self._dropout(src)
+        src = src.transpose(1, 2)
+        
